9cd08b01909e40858c38b2e8f3596bdc33ccf5af
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-13 15:37:20 +0330

    FEAT: add front section of message chat

--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -1,9 +1,10 @@
 # Core imports.
-from django.urls import path
+from django.urls import path, re_path
 
 from .consumers import ChatWebsocketConsumer
 
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', ChatWebsocketConsumer.as_asgi(), name='chat-consumer'),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatWebsocketConsumer.as_asgi()),
+    # path('ws/chat/<str:room_name>/', ChatWebsocketConsumer.as_asgi(), name='chat-consumer'),
 ]
