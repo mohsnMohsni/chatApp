34119e650e4ba17cc3308994b0f2b9d5c16eea93
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-12-13 18:46:12 +0330

    FIX: fix base path of root project

--- core/settings/environments/development.py
+++ core/settings/environments/development.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = 'django-insecure-#d9u0^kz#4l2yej$7p9)&5!h4dprqm$%$(s)nly6wt*%^iitxc'
 
